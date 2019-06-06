415b7f76bc3139b24c6e778a3c30095641160321
Bar Admoni <badmoni@cisco.com>
2019-06-06 23:06:35 +0300

    Improving login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
-import { IonApp, IonButton } from "@ionic/react";
+import { IonButton, IonIcon } from "@ionic/react";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { login } from "../../actions/userAction";
 
+import logo from "../assets/logo.svg";
+
+import "./Login.css";
+
 class Login extends Component {
+  componentWillUnmount() {
+    const classNames = document.body.className.split(" ");
+    document.body.className = classNames
+      .filter(cName => cName !== "login-body")
+      .join(" ");
+  }
+
+  componentDidMount() {
+    document.body.className += " login-body";
+  }
+
   render() {
     if (this.props.user) {
       return <Redirect to="/" />;
     }
+
     return (
-      <Link to="/">
-        <IonButton onClick={this.props.login}> Login </IonButton>
-      </Link>
+      <div className="login-wrapper">
+        <img src={logo} />
+        <br />
+        <div className="login-button-wrapper">
+          <Link to="/">
+            <IonButton onClick={this.props.login} className="login-button">
+              <IonIcon
+                slot="start"
+                name="logo-google"
+                className="login-logo-google"
+              />
+              Connect With Google
+            </IonButton>
+          </Link>
+        </div>
+      </div>
     );
   }
 }
