79aad203446dbd0ecd5ee82ee9de2ae22598f141
cShavit68 <ck20427@gmail.com>
2019-06-10 22:15:28 +0300

    styling page map

--- src/pages/map/MapView.js
+++ src/pages/map/MapView.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import "./MapView.scss";
+
+import back from "../assets/left-arrow.png";
+import circle from "../assets/circle.png";
+import empty from "../assets/empty.png";
+
+
 
 export class MapView extends Component { 
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
 
   render() {
     var points = [
@@ -15,7 +27,17 @@ export class MapView extends Component {
     bounds.extend(points[i]);
   }
     return (
-        <Map google={this.props.google} 
+      <div class="container">
+      <div class="top">
+          <div class="back">
+              <img src={back}/>
+          </div>
+          <div class="title">
+              Trip map
+          </div>
+          </div>
+          <div id="map">
+        <Map className="map-placehplder" google={this.props.google} 
         initialCenter={{
           lat: 48.856613,
           lng: 2.352222
@@ -30,6 +52,23 @@ export class MapView extends Component {
              
           </InfoWindow>
         </Map>
+        <div class="details">
+                    <img src={circle}/>
+                    <div class="current-location">
+                        <span>Current Location</span>
+                        <span></span>
+                        <span class="kilometers"> km </span>
+                    </div>
+                    <div class="routes">
+                            <img src={empty}/>
+                        <div class="route">
+                            nextPlace
+                        </div>
+                    </div>
+                    <div class="bth-my-location">Next Location</div>
+                </div>
+            </div>
+        </div>
       );
     }
 }
