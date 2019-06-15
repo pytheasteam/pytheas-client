f713fa9dee8fc89de7813e62fdb1fddbf9027ab9
ShakedZrihen <shaked.zrihen@gmail.com>
2019-06-15 12:04:58 +0300

    Change to login via server

--- src/api/consts.js
+++ src/api/consts.js
@@ -1,13 +1,13 @@
 // const serverApi =
 //   "http://ec2-35-180-205-235.eu-west-3.compute.amazonaws.com/api";
-// const serverLogin =
-//   "http://ec2-35-180-205-235.eu-west-3.compute.amazonaws.com/login";
+const serverLogin =
+  "http://ec2-35-180-205-235.eu-west-3.compute.amazonaws.com/login";
 const localhostApi = "http://127.0.0.1:5000/api";
 const localhostLogin = "http://127.0.0.1:5000/login";
 
 export const API_BASE = localhostApi;
 
-export const LOGIN_PATH = localhostLogin;
+export const LOGIN_PATH = serverLogin;
 
 export const MAX_PROFILE_COUNT = 4;
 
