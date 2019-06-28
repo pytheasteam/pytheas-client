7ae8fe439e8d6bc7b2ac38992f111fbbc8f06b8e
ShakedZrihen <shaked.zrihen@gmail.com>
2019-06-28 15:24:27 +0300

    Move to dev

--- src/api/consts.js
+++ src/api/consts.js
@@ -1,18 +1,15 @@
 const serverApi = "https://pytheas-server.herokuapp.com/api";
 const serverLogin = "https://pytheas-server.herokuapp.com/login";
 
-const localhostApi = "http://192.168.1.112:5000/api";
-const localhostLogin = "http://192.168.1.112:5000/login";
-
-const myLocalhost = "http://localhost:5000/api";
-const myLocalhostLogin = "http://loaclhost:5000/login";
+const localhostApi = "http://localhost:5000/api";
+const localhostLogin = "http://loaclhost:5000/login";
 
 export const MOCK_MODE = false;
 const DEV_MODE = process.env.REACT_APP_IS_PROD ? false : true;
 
-export const API_BASE = DEV_MODE ? myLocalhost : serverApi;
+export const API_BASE = DEV_MODE ? localhostApi : serverApi;
 
-export const LOGIN_PATH = DEV_MODE ? myLocalhostLogin : serverLogin;
+export const LOGIN_PATH = DEV_MODE ? localhostLogin : serverLogin;
 
 export const MAX_PROFILE_COUNT = 4;
 
